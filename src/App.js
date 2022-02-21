0875e5e06f654e5108748b5e55e4b85519daef6b
Aleksandr Yefremenkov <yefremenkov.alek@gmail.com>
2022-02-21 18:23:50 +0200

    ready form

--- src/App.js
+++ src/App.js
@@ -1,80 +1,59 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Form from "./components/ContactForm/Form";
 import ContactsList from "./components/ContactsList/ContsctsList";
 import Filter from "./components/Filter/Filter";
 import "./App.css";
 
-class App extends Component {
-  state = {
-    contacts: [
+export default function App() {
+  
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const Users = [
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+  ];
+  
+  const [contacts, setContacts] = useState(Users);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const contactsLocal = localStorage.getItem("contactsLocal");
+    const parsedContacts = JSON.parse(contactsLocal);
+
+    setContacts(parsedContacts ?? Users);
+  }, []);
+
+  useEffect(() => { localStorage.setItem("contactsLocal", JSON.stringify(contacts)); }, [contacts]);
+  
+  const handleAddContact = (newContact) => {
+    const filteredUsers = contacts.find((contact) => contact.name === newContact.name);
+    filteredUsers ? alert("Contact is already exist") : setContacts([newContact, ...contacts]);
+  };
+  
+  const handleRemoveContact = (contactId) => {
+    setContacts((contacts) => contacts.filter((contact) => contact.id !== contactId));
   };
 
-  handleAddContact = (newContact) =>
-    this.setState(({ contacts }) => ({
-      contacts: [...contacts, newContact],
-    }));
+  const handleFilterChange = (e) => { setFilter(e.currentTarget.value); };
 
-  handleCheckUniqueContact = (name) => {
-    const { contacts } = this.state;
-
-    const isExistContact = !!contacts.find((contact) => contact.name === name);
-
-    isExistContact && alert("Contact is already exist");
-
-    return !isExistContact;
+  const getVisibleContacts = () => {
+    const lowFilter = filter.toLowerCase();
+    return contacts.filter((contact) => contact.name.toLowerCase().includes(lowFilter));
   };
-
-  handleRemoveContact = (id) =>
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter((contact) => contact.id !== id),
-    }));
-
-  handleFilterChange = (filter) => this.setState({ filter });
-
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
-    return contacts.filter((contact) => contact.name.includes(filter));
-  };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem("contacts");
-    const parsContacts = JSON.parse(contacts);
-      if (parsContacts) {
-      this.setState({contacts:parsContacts})
-      }
-  }
-
-  componentDidUpdate(prevValue, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-  }
-
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
     return (
       <>
         <h2>Form Contact</h2>
         <Form
           className={"forms"}
-          onAdd={this.handleAddContact}
-          onCheckUnique={this.handleCheckUniqueContact}
+          onSubmit={handleAddContact}
         />
         <h2>Contacts list</h2>
-        <Filter filter={filter} onChange={this.handleFilterChange} />
+        <Filter value={filter} onChange={handleFilterChange} />
         <ContactsList
-          contacts={visibleContacts}
-          onRemove={this.handleRemoveContact}
+          contacts={getVisibleContacts}
+          onRemove={handleRemoveContact}
         />
       </>
-    );
-  }
+  );
 }
-
-export default App;
--- src/components/ContactForm/Form.js
+++ src/components/ContactForm/Form.js
@@ -1,73 +1,64 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import shortid from "shortid";
 import s from "./Form.module.css";
 
-class Form extends Component {
-  state = {
-    contacts: [],
-    name: "",
-    number: "",
-  };
+export default function Form({onSubmit}) {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  nameInputId = shortid.generate();
+  const nameInputId = shortid.generate();
+  const numberInputid = shortid.generate();
 
-  handleChange = ({ currentTarget }) => {
+  const handleChange = ({ currentTarget }) => {
     const { name, value } = currentTarget;
 
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { name, number } = this.state;
-    const { onAdd } = this.props;
-
-    const isValidForm = this.validateForm();
-    if (!isValidForm) return;
-    onAdd({ id: shortid.generate(), name, number });
-    this.reset();
-  };
-
-  validateForm = () => {
-    const { name, number } = this.state;
-    const { onCheckUnique } = this.props;
-    if (!name || !number) {
-      alert("Field is empty");
-      return false;
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "number":
+        setNumber(value);
+        break;
+      default:
+        return;
     }
-    return onCheckUnique(name);
   };
 
-  reset = () => {
-    this.setState({ name: "", number: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ id: shortid.generate(), name, number });
+    reset();
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form className={s.form} onSubmit={this.handleSubmit}>
-        <label htmlFor={this.nameInputId}>Name</label>
+  const  reset = () => {
+    setName("");
+    setNumber("");
+  };
+
+  return(
+      <form className={s.form} onSubmit={handleSubmit}>
+        <label htmlFor={nameInputId}>Name</label>
         <input
           placeholder="Your name is..."
           className={s.input}
           type="text"
           name="name"
           value={name}
-          onChange={this.handleChange}
-          id={this.nameInputId}
+          onChange={handleChange}
+          id={nameInputId}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
           required
         />
-        <label htmlFor={this.nameInputId}>Phone</label>
+        <label htmlFor={numberInputid}>Phone</label>
         <input
           placeholder="+..."
           className={s.input}
           type="tel"
           name="number"
           value={number}
-          onChange={this.handleChange}
-          id={this.nameInputId}
+          onChange={handleChange}
+          id={nameInputId}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
           required
@@ -78,5 +69,3 @@ class Form extends Component {
       </form>
     );
   }
-}
-export default Form;
